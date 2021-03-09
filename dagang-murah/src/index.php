d22e6e98d11f50fe9f19a813049403bc89af740b
alxitya <39304181+alxitya@users.noreply.github.com>
2021-03-09 22:43:56 +0700

    Add files via upload

---
+++ dagang-murah/src/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Gerai ZZZ</title>
+        <meta name="description" contents="Disini kami menjual produk produk berkualitas">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+    <HEADER>
+<TITLE>Gerai XYZ</TITLE>
+  <NEXTID N="55">
+  </HEADER>
+  
+    <BODY>
+        <H1>Dagang murah</H1>Murah dan tidak menggantung privasi pengguna disini, kami menjual berbagai macam hal, seperti; produk INI, produk ITU, dan produk SEBagainya.
+        <br>
+        <DL>
+        <DT><A
+        NAME=1 HREF="1.php">Baru pertama kali kesini?</A>
+        <DD> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+        <DT><A
+        NAME=2 HREF="2.php">Dagangan paling laris</A>
+        <DD> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+        <DT><A
+        NAME=3 HREF="3.php">Informasi dari kawan bayangan</A>
+        <DD> Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+        <DT><A
+        NAME=4 HREF="4.php">Orang-orang tersayang</A>
+        <DD>  Pellentesque nec augue a lectus tincidunt tincidunt. Duis et mollis justo. Praesent interdum sapien et justo feugiat mattis. Fusce eleifend vehicula elit non consequat. Nullam accumsan in metus et elementum. Vivamus tincidunt mollis ullamcorper. Donec pharetra efficitur malesuada. Phasellus tempus hendrerit turpis, non porta lectus semper vitae. Etiam imperdiet lectus dui, at interdum ex aliquam vitae. Nulla facilisi. Pellentesque convallis volutpat nisi a tristique. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat.
+
+Sed tincidunt aliquam hendrerit. Cras pellentesque suscipit porta. Quisque a venenatis neque. Duis ligula arcu, viverra sit amet erat vitae, blandit viverra ligula. Etiam eu lorem at sapien porttitor tempus ut nec nulla. Nunc a neque consequat, laoreet nulla sit amet, ultricies sem. Proin varius nulla ut efficitur fringilla. Sed quis elementum massa. Cras rutrum, elit eu rutrum lobortis, tortor quam sollicitudin est, quis condimentum ante purus a ipsum. Proin bibendum velit quis augue commodo, ut ullamcorper mauris porta. 
+        <DT><A
+        NAME=5 HREF="5.php">Sejarah</A>
+        <DD>  Nullam finibus placerat quam, sed mollis augue fermentum sed. Etiam elit est, dictum ut purus vitae, convallis convallis tellus. Curabitur id tempus orci, ac pellentesque massa. Integer sed libero non lacus ornare molestie accumsan id orci. Donec pretium diam pellentesque, ultrices arcu non, maximus nulla. In hac habitasse platea dictumst. Phasellus a lectus ornare, malesuada dui vitae, scelerisque diam. Integer diam purus, sagittis sit amet imperdiet in, euismod id tellus. Sed congue vehicula erat. Morbi id magna id orci placerat convallis. Donec ultricies ultricies justo, in hendrerit sapien interdum at. Integer ornare turpis tempus erat rutrum, eget egestas risus ullamcorper. Vivamus a sem tempus, congue ex sed, porttitor ante. Integer elementum augue nec lacus ornare tincidunt.
+
+Vivamus id semper diam. Nunc vel ipsum accumsan, aliquam nulla fermentum, convallis urna. Aliquam nec feugiat purus, sit amet interdum nisi. Nullam posuere urna quis felis dignissim, ut porta felis sollicitudin. Etiam laoreet eros ut nisl pretium, dignissim mollis orci rutrum. Praesent blandit molestie elit, a cursus dolor ultricies at. Vestibulum in purus nec purus vulputate pretium ac eu lorem. 
+        <DT>
+    </BODY>
+        <footer> 
+          <br>
+          <!-- !!!Website sedang dalam maintenance, tolong jangan kunjungi kami dulu.!!! -->
+        </footer>
+    </body>
+</html>
