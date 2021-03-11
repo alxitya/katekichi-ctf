b2e30a095200d5fa5730c0eb8f1e4d7f34207318
alxitya <39304181+alxitya@users.noreply.github.com>
2021-03-11 15:16:29 +0700

    Delete kamu-tau-cara-debug directory

--- kamu-tau-cara-debug/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url, patterns
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'app.views.index'),
-    url(r'^index$', 'app.views.index'),
-)
